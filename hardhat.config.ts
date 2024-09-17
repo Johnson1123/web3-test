b0d8dcdfa4758299ef7ec70f03bc01bf712355b3
root <root@DESKTOP-3VSLK39>
2024-09-17 13:54:58 -0400

    upload image

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,36 +7,18 @@ require("dotenv").config();
 const config: HardhatUserConfig = {
     solidity: "0.8.24",
     networks: {
-    // for testnet
-    // "lisk-sepolia": {
-    //   url: process.env.LISK_RPC_URL!,
-    //   accounts: [process.env.ACCOUNT_PRIVATE_KEY!],
-    //   gasPrice: 1000000000,
-    // },
-    'lisk-sepolia': {
-      url: process.env.LISK_RPC_URL,
-      accounts: [process.env.ACCOUNT_PRIVATE_KEY as string],
-      gasPrice: 1000000000,
-    },
+      hardhat: {
     forking: {
-        url: process.env.LISK_RPC_URL,
-      }
+        url: process.env.ALCHEMY_RPC_URL as string,
+      }, // Set to "auto" or specify a higher limit
+      gasPrice: 29912165556, // Disable gas price for Hardhat to calculate automatically
+    }
   },
   etherscan: {
     // Use "123" as a placeholder, because Blockscout doesn't need a real API key, and Hardhat will complain if this property isn't set.
     apiKey: {
       "lisk-sepolia": "123",
     },
-    customChains: [
-      {
-        network: "lisk-sepolia",
-        chainId: 4202,
-        urls: {
-          apiURL: "https://sepolia-blockscout.lisk.com/api",
-          browserURL: "https://sepolia-blockscout.lisk.com/",
-        },
-      },
-    ],
   },
   sourcify: {
     enabled: false,
